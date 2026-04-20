ae99a1583e3fb91e2479bd6f3f680fdcf259243c
JustinTrenchcoat <justinchang2021@outlook.com>
2026-04-19 20:39:31 -0400

    modified visualization and R script

--- Script.R
+++ Script.R
@@ -2,23 +2,135 @@
 
 data <- read.csv("data/cleaned.csv")
 
+#standardizing the numerical variables
+data$milage_std <- scale(data$milage)
+data$age_std <- scale(data$age)
+
 ids<-sort(unique(data$fuel_type)) 
 m<-length(ids)
 Y<-list() ; X<-list() ; N<-NULL
-for(j in 1:m) 
+for(j in 1:m)
 {
-  Y[[j]]<-data[data$fuel_type==ids[j], "price"] 
+  Y[[j]]<-data[data$fuel_type==ids[j], "price"]
   N[j]<- sum(data$fuel_type==ids[j])
-  x1<-data[data$fuel_type==ids[j],"milage"] 
+  x1<-data[data$fuel_type==ids[j],"milage_std"] 
   x2<-data[data$fuel_type==ids[j],"accident"]
-  x3<-data[data$fuel_type==ids[j],"age"]
+  x3<-data[data$fuel_type==ids[j],"age_std"]
   X[[j]]<-cbind( rep(1,N[j]), x1,x2,x3  )
 }
+colnames(X[[j]]) <- c("Intercept","milage_std","accident","age_std")
 
 #### OLS fits
 S2.LS<-BETA.LS<-NULL
 for(j in 1:m) {
-  fit<-lm(Y[[j]]~-1+X[[j]] )
+  fit<-lm(Y[[j]]~ -1+X[[j]] )
   BETA.LS<-rbind(BETA.LS,c(fit$coef)) 
   S2.LS<-c(S2.LS, summary(fit)$sigma^2) 
 } 
+
+plot( range(data$age_std),range(data$price),type="n",xlab="age_std", 
+      ylab="log(Price)")
+for(j in 1:m) {    abline(BETA.LS[j,1],BETA.LS[j,4],col="gray")  }
+
+BETA.MLS<-apply(BETA.LS,2,mean)
+abline(BETA.MLS[1],BETA.MLS[4],lwd=2)
+
+plot(N,BETA.LS[,1],xlab="sample size",ylab="intercept")
+abline(h= BETA.MLS[1],col="black",lwd=2)
+plot(N,BETA.LS[,4],xlab="sample size",ylab="slope_age")
+abline(h= BETA.MLS[4],col="black",lwd=2)
+
+#### Hierarchical regression model
+
+## mvnormal simulation
+rmvnorm<-function(n,mu,Sigma)
+{ 
+  E<-matrix(rnorm(n*length(mu)),n,length(mu))
+  t(  t(E%*%chol(Sigma)) +c(mu))
+}
+
+## Wishart simulation
+rwish<-function(n,nu0,S0)
+{
+  sS0 <- chol(S0)
+  S<-array( dim=c( dim(S0),n ) )
+  for(i in 1:n)
+  {
+    Z <- matrix(rnorm(nu0 * dim(S0)[1]), nu0, dim(S0)[1]) %*% sS0
+    S[,,i]<- t(Z)%*%Z
+  }
+  S[,,1:n]
+}
+
+## Setup
+p<-dim(X[[1]])[2]
+theta<-mu0<-apply(BETA.LS,2,mean)
+nu0<-1 ; s2<-s20<-mean(S2.LS)
+eta0<-p+2 
+Sigma<-S0<-L0<-diag(diag(cov(BETA.LS))+1e-6)
+BETA<-BETA.LS
+THETA.b<-S2.b<-NULL
+iL0<-solve(L0)  
+iSigma<-solve(Sigma)
+Sigma.ps<-matrix(0,p,p)
+SIGMA.PS<-NULL
+BETA.ps<-BETA*0
+BETA.pp<-NULL
+set.seed(1)
+mu0[2]+c(-1.96,1.96)*sqrt(L0[2,2])
+
+## MCMC
+for(s in 1:10000) {
+  ##update beta_j 
+  for(j in 1:m) 
+  {  
+    Vj<-solve( iSigma + t(X[[j]])%*%X[[j]]/s2 )
+    Ej<-Vj%*%( iSigma%*%theta + t(X[[j]])%*%Y[[j]]/s2 )
+    BETA[j,]<-rmvnorm(1,Ej,Vj) 
+  } 
+  ##
+  
+  ##update theta
+  Lm<-  solve( iL0 +  m*iSigma )
+  mum<- Lm%*%( iL0%*%mu0 + iSigma%*%apply(BETA,2,sum))
+  theta<-t(rmvnorm(1,mum,Lm))
+  ##
+  
+  ##update Sigma
+  mtheta<-matrix(theta,m,p,byrow=TRUE)
+  iSigma<-rwish(1, eta0+m, solve( S0+t(BETA-mtheta)%*%(BETA-mtheta) ) )
+  ##
+  
+  ##update s2
+  RSS<-0
+  for(j in 1:m) { RSS<-RSS+sum( (Y[[j]]-X[[j]]%*%BETA[j,] )^2 ) }
+  s2<-1/rgamma(1,(nu0+sum(N))/2, (nu0*s20+RSS)/2 )
+  ##
+  ##store results
+  if(s%%10==0) 
+  { 
+     if (s%%100==0){cat(s,s2,"\n")}
+    S2.b<-c(S2.b,s2);THETA.b<-rbind(THETA.b,t(theta))
+    Sigma.ps<-Sigma.ps+solve(iSigma) ; BETA.ps<-BETA.ps+BETA
+    SIGMA.PS<-rbind(SIGMA.PS,c(solve(iSigma)))
+    BETA.pp<-rbind(BETA.pp,rmvnorm(1,theta,solve(iSigma)) )
+  }
+  ##
+}
+
+
+## MCMC diagnostics
+library(coda)
+effectiveSize(S2.b)
+effectiveSize(THETA.b[,1])
+effectiveSize(THETA.b[,2])
+
+apply(SIGMA.PS,2,effectiveSize)
+
+tmp<-NULL;for(j in 1:dim(SIGMA.PS)[2]) { tmp<-c(tmp,acf(SIGMA.PS[,j])$acf[2]) }
+
+acf(S2.b)
+acf(THETA.b[,1])
+acf(THETA.b[,2])
+
+
